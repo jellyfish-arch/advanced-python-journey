042117239cf07f125192bd09e0c19939c14fe06f
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-04-10 23:48:46 +0530

    Created calculator program.

---
+++ foundation/basics/calculator.py
@@ -0,0 +1,18 @@
+def calculator():
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    op = input("Choose (+, -, *, /): ")
+
+    if op == '+':
+        print("Result:", a + b)
+    elif op == '-':
+        print("Result:", a - b)
+    elif op == '*':
+        print("Result:", a * b)
+    elif op == '/':
+        print("Result:", a / b if b != 0 else "Cannot divide by zero")
+    else:
+        print("Invalid operator")
+
+
+calculator()
