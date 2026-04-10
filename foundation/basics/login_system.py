9b2a61e13b837502b33b01540a386a664fb1edf0
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-04-10 23:49:49 +0530

    Created a login system program.

---
+++ foundation/basics/login_system.py
@@ -0,0 +1,10 @@
+username = "admin"
+password = "1234"
+
+u = input("Username: ")
+p = input("Password: ")
+
+if u == username and p == password:
+    print("Login successful")
+else:
+    print("Invalid credentials")
