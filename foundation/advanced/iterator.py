8f7b9e5669ff0953cd9ce15c0c917d17f0b0bf00
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-04-12 23:22:02 +0530

    Created an iterator program.

---
+++ foundation/advanced/iterator.py
@@ -0,0 +1,18 @@
+class Counter:
+    def __init__(self, max):
+        self.max = max
+        self.current = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < self.max:
+            self.current += 1
+            return self.current
+        else:
+            raise StopIteration
+
+
+for num in Counter(5):
+    print(num)
